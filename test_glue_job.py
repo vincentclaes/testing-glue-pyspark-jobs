25f21a5da52aec2b364159b7cbe005b19142b9b2
vincent <vclaes1986@gmail.com>
2019-11-19 20:45:44 +0100

    remove unused code and rename

--- test_glue_job.py
+++ test_glue_job.py
@@ -1,39 +1,68 @@
 import os
 import signal
 import subprocess
-from io import BytesIO
+import unittest
+from unittest import mock
 
 import boto3
-import pandas as pd
 from pyspark.sql import SparkSession
 
-# The os.setsid() is passed in the argument preexec_fn so
-# it's run after the fork() and before  exec() to run the shell.
-pro = subprocess.Popen("moto_server s3", stdout=subprocess.PIPE,
-                       shell=True, preexec_fn=os.setsid)
-
-os.environ["PYSPARK_SUBMIT_ARGS"] = (
-    '--packages "org.apache.hadoop:hadoop-aws:2.7.3" pyspark-shell'
-)
-spark = SparkSession.builder.getOrCreate()
-
-# Setup spark to use s3, and point it to the moto server.
-hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
-hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-hadoop_conf.set("fs.s3a.access.key", "mock")
-hadoop_conf.set("fs.s3a.secret.key", "mock")
-hadoop_conf.set("fs.s3a.endpoint", "http://127.0.0.1:5000")
-
-conn = boto3.resource("s3", endpoint_url="http://127.0.0.1:5000")
-conn.create_bucket(Bucket="bucket")
-
-data = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
-with BytesIO(data.to_csv(index=False).encode()) as buffer:
-    conn.Bucket("bucket").put_object(Key="test/test.csv", Body=buffer)
-
-df = spark.read.csv("s3://bucket/test/test.csv", header=True)
-print(df)
-# DataFrame[a: string, b: string]
+from glue_job import GlueJob
 
 
-os.killpg(os.getpgid(pro.pid), signal.SIGTERM)  # Send the signal to all the process
+class TestGlueJob(unittest.TestCase):
+    S3_MOCK_ENDPOINT = "http://127.0.0.1:5000"
+
+    @classmethod
+    def setUpClass(cls):
+        # setup moto server
+        cls.process = subprocess.Popen("moto_server s3", stdout=subprocess.PIPE,
+                                       shell=True, preexec_fn=os.setsid)
+
+        # create s3 connection, bucket and s3 url's
+        cls.s3_conn = boto3.resource('s3', region_name='eu-central-1', endpoint_url=TestGlueJob.S3_MOCK_ENDPOINT)
+        bucket = 'bucket'
+        cls.s3_conn.create_bucket(Bucket=bucket)
+        cls.s3_source = "s3://{}/{}".format(bucket, 'source.csv')
+        cls.s3_destination = "s3://{}/{}".format(bucket, 'destination.csv')
+
+        # Setup spark to use s3, and point it to the moto server.
+        os.environ["PYSPARK_SUBMIT_ARGS"] = ('--packages "org.apache.hadoop:hadoop-aws:2.7.3" pyspark-shell')
+        cls.spark = SparkSession.builder.getOrCreate()
+        hadoop_conf = cls.spark.sparkContext._jsc.hadoopConfiguration()
+        hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        hadoop_conf.set("fs.s3a.access.key", "mock")
+        hadoop_conf.set("fs.s3a.secret.key", "mock")
+        hadoop_conf.set("fs.s3a.endpoint", TestGlueJob.S3_MOCK_ENDPOINT)
+
+        # create source dataframe and write the dataframe as csv to s3
+        values = [("k1", 1), ("k2", 2)]
+        columns = ['key', 'value']
+        df = cls.spark.createDataFrame(values, columns)
+        df.write.csv(cls.s3_source)
+
+    @mock.patch.object(GlueJob, '_commit_job')
+    @mock.patch.object(GlueJob, '_get_glue_args')
+    @mock.patch.object(GlueJob, '_get_spark_session_and_job')
+    def test_concat_documents_runs_successfully(self, m_glue_session_and_job, m_get_glue_args, m_commit_kob):
+        cli_args = {
+            'source': self.s3_source,
+            'destination': self.s3_destination
+        }
+
+        m_glue_session_and_job.return_value = self.spark, None
+        m_get_glue_args.return_value = cli_args
+        # act
+        GlueJob(cli_args=cli_args, spark=self.spark).run()
+
+        df = self.spark.read.csv(self.s3_destination)
+        self.assertTrue(not df.rdd.isEmpty())
+
+    @classmethod
+    def tearDownClass(cls):
+        # shut down moto server
+        os.killpg(os.getpgid(cls.process.pid), signal.SIGTERM)
+
+
+if __name__ == '__main__':
+    unittest.main()
